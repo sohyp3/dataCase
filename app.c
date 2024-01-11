79e81d46bf343f9710aa98368bd0566ed87e79a6
sohyp3 <0xshb@protonmail.com>
2024-01-11 23:51:43 +0300

    delete old code, new record write _not finished_

--- app.c
+++ app.c
@@ -9,85 +9,6 @@ typedef struct{
 } Record;
 
 
-void write_file_header(FILE *file){
-  char header[1000];
-  memset(header,0,sizeof(header));
-  const char *headerInfo = "HHEADER MASTERHEADER MASTERHEADER MASTERHEADER MASTEREADER MASTER";
-  strncpy(header, headerInfo, strlen(headerInfo));
-
-  fwrite(header, sizeof(header), 1, file);
-//  fclose(file);
-
-}
-
-
-
-void writeTableHeader(FILE *file){
-  
-
-}
-
-
-void write_record(FILE *file, Record *record){
-  fwrite(&(record ->id), sizeof(record->id),1,file);
-  fwrite(record ->data , sizeof(char),sizeof(record->data),file);
-
-}
-
-void read_record(FILE *file, Record *record){
-  fread(&(record->id), sizeof(record->id), 1, file);
-  fread(record->data, sizeof(char), sizeof(record->data), file);
-}
-
-
-int main2(){
-  FILE *file = fopen("data.bin","wb+");
-  if (!file){
-    perror("Err openin file");
-    return EXIT_FAILURE;
-  } 
-
- write_file_header(file);
- Record record = {
-     2000, "new RECORDDDLUGMA LUGMNA!!!!"
-  };
-
-  write_record(file, &record);
-
- Record record2 = {
-     2, "RECORD @!!!!"
-  };
-
-  write_record(file, &record2);
-
-
- Record record3 = {
-     6969, "LAST LIGMA"
-  };
-
-  write_record(file, &record3);
-
-  printf("%lu\n",sizeof(record));
-  printf("%lu\n",sizeof(record2));
-  printf("%lu\n",sizeof(record3));
-
-
-
-
-  Record rRecord;
-  fseek(file, 1208,SEEK_SET);
-  read_record(file, &rRecord);
-
-  printf("id: %d, dat: %s",rRecord.id,rRecord.data);
-
-
-
-  fclose(file);
-
-  return 0;
-}
-
-
 typedef struct{
   char fieldName[50];
   // field Type soon.
@@ -112,6 +33,15 @@ void readTableMetada(FILE *file, tableMetaData *metadata){
 //  fread();
 
 }
+
+void writeRecord(FILE *table_file, Record *record, tableMetaData *metadata){
+  metadata->lastID++;
+  record->id = metadata->lastID;
+  fwrite(&(record->id),sizeof(record->id),1,table_file);
+  fwrite(record->data,sizeof(char), sizeof(record->data),table_file);
+
+}
+      
 
 
 void addTableMetadata(const char *header_file,tableMetaData *metadata){
@@ -163,9 +93,11 @@ void createNewTable(const char *header_file){
 
 
 int main(){
+  /*
   printf("Do you want to create a new table? (y/N) ");
   char new_table_q[2];
   scanf("%s",new_table_q);
+
   if (strcmp(new_table_q,"y") == 0){
     printf("Create New Table mofo");
     createNewTable("header.bin");
@@ -174,6 +106,13 @@ int main(){
   else {
     printf("No New Table mofo");
   }
+  */
+
+  Record record = {
+    .data = "Hello",
+  };
+
+  
 
   return 0;
 
